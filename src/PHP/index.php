1f49eee85b5e4895899f9b022bcd15f581866a63
RyanGouldsmith <GouldsmithRyan@hotmail.co.uk>
2014-02-20 13:55:33 +0000

    added the html5 structure

---
+++ src/PHP/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<head>
+	<title> Web Formula </title>
+</head>
+<body>
+	<main>
+		<header>
+			<h1> Web Forumla </h1>
+			<p> Find all your math formula in an easy location </p>
+		</header>
+		<section> 
+			<p> Input into the search function to see the formula </p>
+		</section>
+	</main>
+</body>
