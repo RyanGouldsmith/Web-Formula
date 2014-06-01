ff060948d46fcbc0e71a8ee2742f007024cc39d7
RyanGouldsmith <GouldsmithRyan@hotmail.co.uk>
2014-06-01 19:48:15 +0100

    Working version of the app

--- src/php/dbhandler.php
+++ src/php/dbhandler.php
@@ -5,32 +5,55 @@
 		private $password = "";
 		private $host = ""; 
 		private $database = "";
-		private $table_post = "";
-		private $connection;		
+		private $table = "";
+		private $connection;
+		private $array_results = array();
+		private $stripped_query = "";
+		
 		/**
 		 * Makes the initial connection to the database, by taking the private member fields as parmeters
 		 */
 		public function __construct(){
-				$this->connection = mysqli_connect($this->host, $this->username, $this->password, $this->database);
+				$this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
 		}
 		/** 
-		 * Send the Query to the Database
+		 * Sets the table that is to be used 
+		 * @param tablename is the table in which will be used
 		 */
-		public function send_query_to_db($query){
-				$this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
+		public function select_table($tablename){
+				$this->table = $tablename;
+		}
+		/** 
+		 * @returns the table which is currently in use.
+		 */
+		public function get_table(){
+			return $this->table;
 		}
 		/** 
 		 * Send the Query to the Database
 		 * If there's an error it will throw a new error warning
 		 * @param query is the query which we will want to interact with the database.
+		 * @return an array of associative array items, which stores all the equation title and the equation itself.
 		 */
 		public function send_query_to_db($query){
-				if(!$this->connection->query($query)){
-				     echo ("An error has occured: ", $this->connection->error);
-			    }				
+				if($result = $this->connection->query($query)){
+					while ($values = $result->fetch_assoc()){
+							$this->array_results[] = $values;
+					}
+					return $this->array_results;
+				} else { 
+					echo "An error has occured " . $this->connection->error;
+				}
 		}
-	
+		/** 
+		 * Prevents any SQL injections and sanitises it so it allows for safe use in the database
+		 * @param query is the part in which we will sanitising
+		 */
+		public function sanitise_query($query){
+			$this->stripped_query = $this->connection->real_escape_string($query);
+		}
+		public function get_stripped_query(){
+				return $this->stripped_query;
+		}
 	}
-	//closes any connection when it finishes
-	//$this->connection->close();
 ?>
--- src/php/index.php
+++ src/php/index.php
@@ -2,7 +2,15 @@
 <head>
 	<title> Web Formula </title>
 	<link rel="stylesheet" type="text/css" href="../css/style.css">
-	<script type="text/javascript"src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+	<script type="text/javascript"src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
+	MathJax.Hub.Config({
+  tex2jax: {
+    inlineMath: [['$','$'], ['\\(','\\)']],
+    processEscapes: true
+  }
+});
+	</script>
+	
 </head>
 <body>
 	<main>
@@ -13,9 +21,32 @@
 		<section> 
 			<p> Input into the search function to see the formula </p>
 			<form method="get" action ="">
-				<input type="text" placeholder="Enter the Forumla"> </input>
+				<input type="text" placeholder="Enter the Forumla" id="formula" name="formula"> </input>
 				<input type="Submit"></input>
 			</form>
 		</section>
+		<?php
+			include 'dbhandler.php';
+			include 'equation.php';
+			$connection = new dbhandler();
+			if(isset($_GET['formula'])){
+				$connection->sanitise_query($_GET['formula']);
+				$query = "SELECT EquationName, EquationValue FROM ".$connection->get_table()." WHERE EquationName='".$connection->get_stripped_query()."'";
+				$result = $connection->send_query_to_db($query);
+				for ($i = 0; $i < count($result); $i++){
+					foreach($result[$i] as $key => $value){
+						echo "<div id='showequation'>";
+							if ($key == "EquationValue"){
+								echo"<p>$". $value . "$</p>";
+							}			
+							else { 
+								echo "<p>" . $value . "</p>";
+							}
+						echo "</div>";	
+					}
+				}
+			}
+			$connection->connection->close();
+		?>
 	</main>
 </body>
